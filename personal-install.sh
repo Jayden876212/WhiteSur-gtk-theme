745a1d9a88198a59d972c0e943e401ee2ce389af
Jayden876212 <dxmqkgax@duck.com>
2025-01-14 14:48:54 +0000

    Fix install script

--- personal-install.sh
+++ personal-install.sh
@@ -1,1 +1,1 @@
-./install.sh --nautilus-style glassy --color Dark --theme blue --nord --libadwaita
+./install.sh --nautilus-style glassy --color Dark --theme blue --nord --libadwaita --opacity normal --name "WhiteSur-tdljayden-nordic"
